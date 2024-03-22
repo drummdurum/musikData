1225a08b73854d6322b8efe5138804bf6ff4677d
drummdurum <147395124+drummdurum@users.noreply.github.com>
2024-03-22 11:22:02 +0100

    første commit

---
+++ src/main/java/com/example/musikdata/MusikDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.musikdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusikDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musikdata/MusikDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musikdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
